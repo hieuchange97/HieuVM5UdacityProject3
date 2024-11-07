796387cd17c599a351de1f7df26e5250a52804ef
Hieu Vu Minh <hieuchange97@gmail.com>
2024-11-07 16:40:44 +0700

    Modify send notification

--- app/routes.py
+++ app/routes.py
@@ -67,24 +67,8 @@ def notification():
             db.session.add(notification)
             db.session.commit()
 
-            ##################################################
-            ## TODO: Refactor This logic into an Azure Function
-            ## Code below will be replaced by a message queue
-            #################################################
-            attendees = Attendee.query.all()
-
-            for attendee in attendees:
-                subject = '{}: {}'.format(attendee.first_name, notification.subject)
-                send_email(attendee.email, subject, notification.message)
-
-            notification.completed_date = datetime.utcnow()
-            notification.status = 'Notified {} attendees'.format(len(attendees))
-            db.session.commit()
-            # TODO Call servicebus queue_client to enqueue notification ID
-
-            #################################################
-            ## END of TODO
-            #################################################
+            # Call service bus queue_client to send notification id to queue for function app can catch and send email
+            queue_client.send(Message('{}'.format(notification)))
 
             return redirect('/Notifications')
         except :
@@ -92,7 +76,6 @@ def notification():
 
     else:
         return render_template('notification.html')
-
 
 
 def send_email(email, subject, body):
